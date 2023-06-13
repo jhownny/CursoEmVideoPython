b5474c115e4a7de8d6672337a4b0f7ec2345cace
Jhonata Batista <88687538+jhownny@users.noreply.github.com>
2023-06-12 21:00:46 -0300

    Commit

--- CursoEmVideo-Python/PythonExercicios/Atividade de 61 a 70/ex066.py
+++ CursoEmVideo-Python/PythonExercicios/Atividade de 61 a 70/ex066.py
@@ -6,9 +6,28 @@
 # B) Quantos homens foram cadastrados.
 # C) Quantas mulheres tem menos de 20 anos.
 
-sexo = ""
-idade = 0
+cont_idade = 0
+cont_sexo = 0
+cont_womanold = 0
 
 while True:
-    sexo = str.upper(input('Digite seu Sexo (Masculino ou Feminino): '))
-    
+    sexo = str.capitalize(input('Digite seu Sexo (Masculino ou Feminino): '))
+    idade = int(input('Quantos anos você tem: '))
+    print('====================================================')
+    if idade > 18:
+        cont_idade+=1
+        if sexo == "Masculino":
+            cont_sexo+=1
+        elif sexo == "Feminino" and idade < 20:
+            cont_womanold+=1
+    else:
+        if sexo == "Masculino":
+            cont_sexo+=1
+        elif sexo == "Feminino" and idade < 20:
+            cont_womanold+=1
+    Again = str.capitalize(input('Deseja continuar? \n:'))
+    if Again == "Não":
+        break
+print('\n====================================================')
+print(f'Teve {cont_idade} pessoa(s) com mais de 18 anos \n{cont_sexo} Homen(s) cadastrado(s) \n{cont_womanold} Mulher(es) com menos de 20 anos.')
+print('====================================================\n')
--- CursoEmVideo-Python/PythonExercicios/Atividade de 71 a 80/ex068.py
+++ CursoEmVideo-Python/PythonExercicios/Atividade de 71 a 80/ex068.py
@@ -3,3 +3,4 @@
 # programa vai informar quantas células de cada valor serão entregues.
 #
 # OBS: Considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
+
